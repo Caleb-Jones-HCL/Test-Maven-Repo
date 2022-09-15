2c0b03ee897b5164c0cf609bb5b08d7c3719c776
unknown <caleb.jones@hcl.com>
2022-09-15 15:23:44 -0500

    initial commit

---
+++ src/main/java/calebTestMaven/testApp.java
@@ -0,0 +1,10 @@
+package calebTestMaven;
+
+public class testApp {
+
+	public static void main( String[] args )
+    {
+        System.out.println( "Hello  YES CHANGES World!" );
+        System.out.println("This is my new maven project. ");
+    }
+}
